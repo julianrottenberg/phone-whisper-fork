239e6405d0970680d347d803243029dbadfec49e
julianrottenberg <julianrottenberg@gmail.com>
2026-08-15 15:45:23 +0200

    Release v0.3.3: reasoning effort setting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.kafkasl.phonewhisper"
         minSdk = 30
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.3.2"
+        versionCode = 5
+        versionName = "0.3.3"
 
         ndk { abiFilters += "arm64-v8a" }
     }
